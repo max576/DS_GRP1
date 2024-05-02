0132c9c0cb35088f4c5a9b2fa7a1fe34958d2cde
Greg Gibbs <33558582+gregoxx@users.noreply.github.com>
2024-05-02 12:58:27 +0100

    Update Vader.py

--- Vader.py
+++ Vader.py
@@ -1,4 +1,25 @@
 import pandas as pd
+
+Youtube_trump_df = pd.read_csv(r"/kaggle/input/trumpyt/all_trump_youtube_comments.csv")
+
+Youtube_biden_df = pd.read_csv(r"/kaggle/input/youtubecomments/youtube/all_biden_youtube_comments.csv",lineterminator='\n')
+
+Impeached_pd = pd.read_csv("/kaggle/working/impeachment.csv")
+
+Afghanistan_pd = pd.read_csv("/kaggle/input/afghanistan/afghanistan_comments.csv")
+
+TrumpImpeachmentSpeach = pd.read_csv("/kaggle/working/TrumpImpeachmentSpeech.csv")
+
+Riots = pd.read_csv("/kaggle/input/trump-riots-and-tweets/capitol_riots_tweets.csv")
+
+TrumpTweets = pd.read_csv("/kaggle/input/trump-riots-and-tweets/trump_tweets.csv")
+
+trump_reelection = pd.read_csv("/kaggle/input/reelection/trump_reelection.csv")
+
+biden_reelection = pd.read_csv("/kaggle/input/reelection/biden_reelection.csv")
+
+
+import pandas as pd
 import re
 from nltk.sentiment import SentimentIntensityAnalyzer
 
